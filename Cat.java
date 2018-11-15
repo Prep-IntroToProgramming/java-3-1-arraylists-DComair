cae5ad837546f2ba70bb0d433e0d7505a4454d4b
Daniel Comer <19ComerD@flintridgeprep.org>
2018-11-15 09:52:00 -0800

    Made Cat class, began Test

--- Cat.java
+++ Cat.java
@@ -1,3 +1,25 @@
 
 public class Cat {
+    String name;
+    double weight;
+    
+    String getName(){
+        return name;
+    }
+    
+    double getWeight(){
+        return weight;
+    }
+    
+    //empty cat constructor
+    Cat(){        
+    }
+    //overloaded cat constructor to assign name
+    Cat(String newName){
+        name = newName;
+    }
+    
+    void meow(){
+        System.out.println("Meow");
+    }
 }
