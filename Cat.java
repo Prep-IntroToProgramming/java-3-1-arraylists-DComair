548452e18494ca538a4774b03d918cabfa5ffeff
Daniel Comer <19ComerD@flintridgeprep.org>
2018-11-16 11:08:07 -0800

    Added bonus 1

--- Cat.java
+++ Cat.java
@@ -2,19 +2,19 @@
 public class Cat {
     String name;
     double weight;
-    
+
     String getName(){ //gets the cat's name
         return name;
     }
-    
+
     double getWeight(){ //gets the cat's weight
         return weight;
     }
-    
+
     void setWeight(double newWeight){ //sets the cat's weight
         weight = newWeight;
     }
-    
+
     //empty cat constructor
     Cat(){        
     }
@@ -22,8 +22,16 @@ public class Cat {
     Cat(String newName){
         name = newName;
     }
-    
+
     void meow(){
-        System.out.println("Meow");
+        //(int) casts it to an integer, meowChance can be 1, 2, 3, or 4
+        int meowChance = (int)(Math.random() * 4) + 1;
+        //One in four chance to do the new code
+        if (meowChance == 4){
+            System.out.println("Bork");
+        }
+        else {
+            System.out.println("Meow");
+        }        
     }
 }
